970c069dcf112d2985b29cba938d8d79ef67d74d
Ivan Sorokin <unxed@mail.ru>
2026-03-14 16:00:35 +0100

    Remove visual glitches in VT

--- ansi_parser.go
+++ ansi_parser.go
@@ -38,33 +38,63 @@ func (p *AnsiParser) Process(data []byte) {
 		case StateGround:
 			if b == 0x1b {
 				p.State = StateEsc
+			} else if b == 0x07 {
+				// BEL - often used as OSC terminator
+				p.State = StateGround
 			} else {
 				p.term.PutChar(rune(b), p.Attr)
 			}
 		case StateEsc:
-			if b == '[' {
+			switch b {
+			case '[':
 				p.State = StateCSI
 				p.Params = nil
 				p.CurParam.Reset()
-			} else {
+			case ']':
+				p.State = StateOSC
+			case '(', ')':
+				// Character set selection, swallow next byte
+				p.State = StateGround
+			default:
 				p.State = StateGround
 			}
 		case StateCSI:
-			if b >= '0' && b <= '9' {
-				p.CurParam.WriteByte(b)
-			} else if b == ';' {
-				p.Params = append(p.Params, p.CurParam.String())
-				p.CurParam.Reset()
+			// Parameter bytes: 0x30–0x3F (0-9 : ; < = > ?)
+			if b >= 0x30 && b <= 0x3F {
+				if b == ';' {
+					p.Params = append(p.Params, p.CurParam.String())
+					p.CurParam.Reset()
+				} else {
+					p.CurParam.WriteByte(b)
+				}
 			} else {
+				// Final byte: 0x40–0x7E
 				p.Params = append(p.Params, p.CurParam.String())
 				p.handleCSI(b)
 				p.State = StateGround
 			}
+		case StateOSC:
+			// Operating System Command: ends with BEL (0x07) or ST (Esc \)
+			if b == 0x07 {
+				p.State = StateGround
+			} else if b == 0x1b {
+				p.State = StateEsc
+			}
+			// Just swallow all OSC content for now
 		}
 	}
 }
 
 func (p *AnsiParser) handleCSI(cmd byte) {
+	// Filter out prefixes like '?' from the first parameter if present
+	firstParam := ""
+	if len(p.Params) > 0 {
+		firstParam = p.Params[0]
+		if len(firstParam) > 0 && (firstParam[0] == '?' || firstParam[0] == '>') {
+			firstParam = firstParam[1:]
+		}
+	}
+
 	switch cmd {
 	case 'm': // SGR
 		for _, s := range p.Params {
@@ -73,6 +103,9 @@ func (p *AnsiParser) handleCSI(cmd byte) {
 		}
 	case 'H', 'f': // Cursor position
 		row, col := 1, 1
+		if firstParam != "" {
+			row, _ = strconv.Atoi(firstParam)
+		}
 		if len(p.Params) > 0 && p.Params[0] != "" {
 			row, _ = strconv.Atoi(p.Params[0])
 		}
